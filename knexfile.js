617d7aa2a212fd75ea174a6bae2fff882ebd7a0b
amlane <twelvexstring@gmail.com>
2019-06-25 23:24:55 -0500

    pushing all changes to github

--- knexfile.js
+++ knexfile.js
@@ -16,5 +16,21 @@ module.exports = {
     seeds: {
       directory: "./data/seeds"
     }
+  },
+  production: {
+    client: "pg",
+    useNullAsDefault: true,
+    connection: process.env.DATABASE_URL,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
   }
 };
