70e8e9741c8d7d4de7f0a48bdef7ac30cd6e7833
amlane <twelvexstring@gmail.com>
2019-07-01 22:35:54 -0500

    whoops wrong error message

--- middleware/restricted-middleware.js
+++ middleware/restricted-middleware.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
   if (token) {
     jwt.verify(token, secrets.jwtSecret, err => {
       if (err) {
-        res.status(401).json({ message: "Incorrect Username or Password" });
+        res.status(401).json({ message: "Invalid Credentials" });
       } else {
         next();
       }
